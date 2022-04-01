05bcaf29f8cb2ff463050f58c99bd67197990245
drawishe <fogreen7@gmail.com>
2022-04-02 00:03:11 +0300

    furman_beta2

--- Furman/task3.py
+++ Furman/task3.py
@@ -1,8 +1,16 @@
+# Импорты: 
 import matplotlib.pyplot as plt
-# Создание массива под автомат А'
 import numpy as np
+import pydot
+import os
+from graphviz import Digraph
+
+# Создание массива под автомат А'
 arr=np.zeros(75, int).reshape(5,5,3)
-arr
+print('Вывод нулевого массива: ')
+print(arr)
+print('Старт программы')
+
 
 # Добавление весов и направлений в автомат А', направления указаны от строки к столбцам, 
 # q1'=0 элементу массива, q5'=4 элементу массива
@@ -22,19 +30,28 @@ arr[3][4][1]=3
 arr[4][2][0]=3
 arr[4][3][0]=2
 arr[4][4][0]=1
-arr
+print('Вывод заполненного массива: ')
+print(arr)
 
 # Отрисовка автомата А', выбор финальных состояний
-from graphviz import Digraph
+
 graph1= Digraph(comment="Автомат А'")
 # Создание вершин q1-q5
 colors=['red']
 
-a=int(input('q1 конечное состояние? 1 - да, 0 - нет: '))
-b=int(input('q2 конечное состояние? 1 - да, 0 - нет: '))
-c=int(input('q3 конечное состояние? 1 - да, 0 - нет: '))
-d=int(input('q4 конечное состояние? 1 - да, 0 - нет: '))
-e=int(input('q5 конечное состояние? 1 - да, 0 - нет: '))
+# a=int(input('q1 конечное состояние? 1 - да, 0 - нет: '))
+# b=int(input('q2 конечное состояние? 1 - да, 0 - нет: '))
+# c=int(input('q3 конечное состояние? 1 - да, 0 - нет: '))
+# d=int(input('q4 конечное состояние? 1 - да, 0 - нет: '))
+# e=int(input('q5 конечное состояние? 1 - да, 0 - нет: '))
+
+# Заполнение стоковыми значениями
+a=0
+b=0
+c=1
+d=0
+e=1
+
 
 if a==1:
     graph1.node('0','q1',fillcolor="red",style="filled")
@@ -66,17 +83,21 @@ for i in range(0,5):
             if arr[i][j][z] != 0:
                 temp = arr[i][j][z]
                 temp = temp-1
-                graph1.edge('%s'%(i),'%s'%(j),'%s'%(temp))
+                graph1.edge(f'{i}',f'{j}',f'{temp}')
                 
 # print(graph1.source) 
-import pydot
-graph1.render('graph1.gv', view=True)
-graph1
-# arr
+
+graph1.save('graph1.dot', None)
+(graph1,) = pydot.graph_from_dot_file('graph1.dot')
+graph1.write_png('graph1.png')
+# display(Image(filename='graph1.png'))
+os.remove('graph1.dot')
+
+# print('Вывод массива: ')
+# print(arr)
 
 
 # Отрисовка автомата А' с дополнененным языком
-from graphviz import Digraph
 graph1= Digraph(comment="Автомат А'")
 # Создание вершин q1-q5
 colors=['red']
@@ -110,11 +131,18 @@ for i in range(0,5):
             if arr[i][j][z] != 0:
                 temp = arr[i][j][z]
                 temp = temp-1
-                graph1.edge('%s'%(i),'%s'%(j),'%s'%(temp))
+                graph1.edge(f'{i}',f'{j}',f'{temp}')
                 
 
 
 # print(graph1.source) 
-graph1.render('graph1.gv', view=True)
-graph1
-# arr
+graph1.save('graph2.dot', None)
+(graph1,) = pydot.graph_from_dot_file('graph2.dot')
+graph1.write_png('graph2.png')
+os.remove('graph2.dot')
+# print('Вывод массива: ')
+# print(arr)
+
+
+
+print('Завершение программы')
